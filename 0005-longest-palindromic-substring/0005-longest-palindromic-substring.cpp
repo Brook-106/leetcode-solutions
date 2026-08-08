88cf0967d7a4186af01cf732008dd7b240cc9e45
Brook-106 <ogbrookriyal@gmail.com>
2026-08-08 19:06:59 +0530

    Time: 4 ms (93.73%), Space: 9.6 MB (49.08%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+
+        int start = 0, maxLen = 1;
+
+        auto expandAroundCenter = [&](int left, int right) {
+            while (left >= 0 && right < s.size() && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            int len = right - left - 1;
+            if (len > maxLen) {
+                maxLen = len;
+                start = left + 1;
+            }
+        };
+
+        for (int i = 0; i < s.size(); i++) {
+            expandAroundCenter(i, i);      
+            expandAroundCenter(i, i + 1);   
+        }
+
+        return s.substr(start, maxLen);
+    }
+};
